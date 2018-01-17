bef3924794696ab5fc151366151fadb896374e98
Anukul <anukul.bhatnagar@tcs.com>
2018-01-17 18:55:41 +0800

    Testing

--- BookStore_Clean/DataLayer/Service/BookService/BookService.swift
+++ BookStore_Clean/DataLayer/Service/BookService/BookService.swift
@@ -34,7 +34,7 @@ struct BookService: BookServiceProtocol {
         let book = dbService.createEntity(entity: Book.self, onContext: dbService.mainContext)
         book?.bookName = bookName
         book?.bookID = bookID
-        //Changes
+        //Changes New
         dbService.saveContext()
     }
     
